768eec9933415cb0ebdac2fd3cd93ab8a15c1f50
Semyon Suslin <s.suslin@uiscom.ru>
2018-09-06 16:26:55 +0300

    start lada lead test(хотелка)

---
+++ lada_lead_test/personal_form.js
@@ -0,0 +1,38 @@
+Comagic.UI.registerViewController('personal_form', function (settings, tpls) {
+    var intervalId = setInterval(function () {
+        var sitephone = Comagic.UI.getWidget('sitephone');
+        var cache = {};
+        if (sitephone) {
+            clearInterval(intervalId);
+            var personalForm = Comagic.UI.createWidget('personal_form', {
+                settings: settings,
+                id: settings['id'],
+                template: tpls['personal_form']
+            });
+
+            if (settings.image_url) {
+                cache.image_url = settings.image_url;
+                sitephone.refs.img.style = 'background-image: url(' + settings.image_url + ')';
+            }
+            cache.title = sitephone.refs.title.innerHTML;
+            cache.text = sitephone.refs.text.innerHTML;
+            cache.submitBtn = sitephone.refs.submitBtn.innerHTML;
+            
+            sitephone.refs.title.innerHTML = settings.banner_title;
+            sitephone.refs.text.innerHTML = settings.banner_text;
+            sitephone.refs.submitBtn.innerHTML = settings.button_text;
+
+            sitephone.show();
+            sitephone.refs['closeBtn'].addEventListener('click', function () {
+                personalForm.cancel();
+                sitephone.refs.title.innerHTML = cache.title;
+                sitephone.refs.text.innerHTML = cache.text;
+                sitephone.refs.submitBtn.innerHTML = cache.submitBtn;
+            });
+
+            sitephone.on('callcomplete', function () {
+                personalForm.submit();
+            });
+        }
+    }, 1000);
+});
